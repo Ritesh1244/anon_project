59bfb82673fc712e2e71615328cfc90c0b937fda
Ritesh617 <ritesh21100@iiitnr.edu.in>
2024-12-23 21:51:49 +0530

    index.ts -not major chnage

--- src/index.ts
+++ src/index.ts
@@ -14,12 +14,23 @@ async function main() {
 
   try {
     await driver.get("https://meet.google.com/wfq-ywbe-joi");
-    
-    // await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
-    await driver.wait(
-      until.titleIs("hklkhjlh - Google Search"),
-      10000
-    );
+    // await driver.findElement(By.id("c5")).sendKeys("value","your value")
+    // await driver.wait(
+    //   until.titleIs("hklkhjlh - Google Search"),
+    //   1000000
+    // );
+
+    await driver.sleep(3000);
+    const popButton = await driver.wait(until.elementLocated(By.xpath('//span[contains(text(),"Got it")]')),1000);
+    popButton.click()
+    const nameInput = await driver.wait(until.elementLocated(By.id('c5')),10000);
+    await nameInput.clear();
+    await nameInput.click()
+    await nameInput.sendKeys('value',"Metting bot")
+    const buttonInput = await driver.wait(until.elementLocated(By.xpath('//span[contains(text(),"Ask to join") or contains(text(),"Join"))]')),1000);
+    buttonInput.click()
+    const nameInput2 = await driver.wait(until.elementLocated(By.id('c55433')),10000);
+
   } finally {
     await driver.quit();
   }
